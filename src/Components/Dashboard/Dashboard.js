9706470b62024ba8878daff27fb4bf5ef1c30065
Mark_Gara <garabilezmark0130@gmail.com>
2024-08-02 14:14:09 +0800

    message

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import './Dashboard.css';
-
-// function Dashboard() {
-//   return (
-//     <div className="dashboard">
-//       <div className="dashboard-header">
-//         <div class="left-head">
-//           <div class="head-logo"><i class="fa-solid fa-circle"></i> &nbsp; </div>
-//           <div class="head-atec">
-//               <span>ATEC</span> &nbsp; TECHNOLOGICAL COLLEGE
-//           </div>
-//         </div>
-//         <div class="right-head">
-//           <div class="prof-icon"><i class="fa-regular fa-user"></i></div>
-//           <div class="prof-name">Juan Dela Cruz</div>
-//           <div class="drop"><i class="fa-solid fa-angle-right"></i></div>
-//         </div>
-//       </div>
-//       <div className="dashboard-panels-wrapper">
-//         <div className="side-panel-L">
-//           <div class="in-panel room-sched"></div>
-//           <div class="in-panel your-sched"></div>
-//           <div class="in-panel logbook"></div>
-//           <div class="in-panel rfid-reg"></div>
-//         </div>
-//         <div className="dashboard-panels">
-//           <div className="panel">Panel 1</div>
-//           <div className="panel">Panel 2</div>
-//           <div className="panel">Panel 3</div>
-//           <div className="panel">Panel 4</div>
-//         </div>
-//         <div className="side-panel-R">
-//           Side Panel 2
-//         </div>
-//       </div>
-//       <div className="dashboard-footer">
-//         Footer
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Dashboard;
 
 import React from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
@@ -71,82 +27,6 @@ const App = () => {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   return (
-<<<<<<< HEAD
-    <Layout>
-      <Header
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={items1}
-          style={{
-            flex: 1,
-            minWidth: 0,
-          }}
-        />
-      </Header>
-      <Content
-        style={{
-          padding: '0 48px',
-        }}
-      >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
-          }}
-        >
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <Layout
-          style={{
-            padding: '24px 0',
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Sider
-            style={{
-              background: colorBgContainer,
-            }}
-            width={200}
-          >
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{
-                height: '100%',
-              }}
-              items={items2}
-            />
-          </Sider>
-          <Content
-            style={{
-              padding: '0 24px',
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Content>
-      <Footer
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
-    </Layout>
-=======
     <div className="dashboard">
       <div className="dashboard-header">
         <div class="left-head">
@@ -207,7 +87,6 @@ const App = () => {
         Footer
       </div>
     </div>
->>>>>>> d701a6335625924f9e7c9a0cfb50d7df876b84dc
   );
 };
 export default App;
