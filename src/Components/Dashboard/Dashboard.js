d701a6335625924f9e7c9a0cfb50d7df876b84dc
Justine Arandia <justine.arandia.m@bulsu.edu.ph>
2024-08-02 12:42:11 +0800

    dashboard update

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,9 +6,9 @@ function Dashboard() {
     <div className="dashboard">
       <div className="dashboard-header">
         <div class="left-head">
-          <div class="head-logo"><i class="fa-solid fa-circle"></i> &nbsp; </div>
+          <div class="head-logo"></div>
           <div class="head-atec">
-              <span>ATEC</span> &nbsp; TECHNOLOGICAL COLLEGE
+            &nbsp; &nbsp; <span>ATEC</span> &nbsp; TECHNOLOGICAL COLLEGE
           </div>
         </div>
         <div class="right-head">
@@ -19,13 +19,41 @@ function Dashboard() {
       </div>
       <div className="dashboard-panels-wrapper">
         <div className="side-panel-L">
-          Side Panel 1
+          <div class="menu-panel dash">
+            <i class="fa-solid fa-house"></i>
+            Dashboard
+          </div>
+          <div class="menu-panel room">
+          <i class="fa-solid fa-calendar-days"></i>
+            Room Schedule
+          </div>
+          <div class="menu-panel log">
+          <i class="fa-solid fa-book-open"></i> 
+            Logbook
+          </div>
+          <div class="menu-panel rfid">
+          <i class="fa-brands fa-cc-diners-club"></i>
+            RFID Registration
+          </div>
+          <div class="menu-panel notif">
+          <i class="fa-solid fa-bell"></i> 
+            Notification
+          </div>
         </div>
-        <div className="dashboard-panels">
-          <div className="panel">Panel 1</div>
-          <div className="panel">Panel 2</div>
-          <div className="panel">Panel 3</div>
-          <div className="panel">Panel 4</div>
+        <div className="content-panel">
+          <div className="sect bread">Dashboard /  Home</div>
+          <div className="sect ">Panel 1</div>
+          <div className="sect ">Panel 2</div>
+          <div className="dash-panels">
+            <div class="panel">
+              
+            </div>
+            <div class="panel"></div>
+            <div class="panel"></div>
+            <div class="panel"></div>
+            <div class="panel"></div>
+          </div>
+          <div className="sect ">Panel 3</div>
         </div>
         <div className="side-panel-R">
           Side Panel 2
