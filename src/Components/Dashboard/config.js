f24e55ee55284b7260860126c51811187b8f0022
Mark_Gara <garabilezmark0130@gmail.com>
2024-11-17 18:01:51 +0800

    domain backend provided

--- src/Components/Dashboard/config.js
+++ src/Components/Dashboard/config.js
@@ -3,7 +3,7 @@
 
 // export const API_BASE_URL = 'http://192.168.1.103:8000';
 
-export const API_BASE_URL = 'https://star-snapper-nationally.ngrok-free.app';
+export const API_BASE_URL = 'https://server.smartsilid.site';
 
 //export const API_BASE_URL = 'https://5009-120-28-251-222.ngrok-free.app';
 
